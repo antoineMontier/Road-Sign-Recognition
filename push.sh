1e56fe5988f218c19bde033b5368cc6cd4384ee9
Antoine <antoine.montier@sfr.fr>
2023-10-17 11:47:19 +0200

    initialized repo

---
+++ push.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# pre-activation command : chmod 777 push.sh
+
+function git_add_commit_push() {
+    if (($# == 0)) ; then
+        echo "error, use this way: './push.sh my git commit message'"
+    else
+        git add *
+
+        commit_message="git commit -m '"
+        for word in "$@" ; do
+            commit_message="$commit_message $word"
+        done
+        commit_message="$commit_message'"
+
+        eval "$commit_message"
+        git push 
+    fi
+}
+
+git_add_commit_push "$@"
