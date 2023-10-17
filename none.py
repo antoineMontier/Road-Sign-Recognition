dd489a59c2a18db5d9cf7c093f803675f2bd46b4
Antoine <antoine.montier@sfr.fr>
2023-10-17 22:15:45 +0200

    tried to find the position of the sign using measure.regionprops... not working

--- none.py
+++ none.py
@@ -15,6 +15,24 @@ import scipy
 for i in range(1, 172):
     fn = f"./img/IMG_0{i:03d}.png"
     img = cv2.imread(fn)
+
+    img_hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+    lower_red = np.array([0, 40, 40])
+    upper_red = np.array([12, 255, 255])
+
+    mask = cv2.inRange(img_hsv, lower_red, upper_red)
+
+    red_segmented = cv2.bitwise_and(img, img, mask=mask)
+
+    img = cv2.cvtColor(red_segmented,cv2.COLOR_HSV2BGR)
+    img = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+
+
+    plt.imshow(img)
+    plt.show()
+
+    continue
+
     img_hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
     lower_red = np.array([0, 40, 40])
     upper_red = np.array([12, 255, 255])
