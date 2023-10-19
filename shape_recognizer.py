e136ac1764d630adbd18ce02f3da1b2112568abe
Antoine <antoine.montier@sfr.fr>
2023-10-19 11:23:53 +0200

    worked on shape recognition of the contours obtained

--- shape_recognizer.py
+++ shape_recognizer.py
@@ -2,25 +2,34 @@ import os
 import numpy as np
 import cv2
 import matplotlib
+import random
 from matplotlib import image
 from sklearn.model_selection import train_test_split
 from keras.utils import to_categorical
+from tensorflow.keras.applications import MobileNetV2
 from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense, Conv2D, Conv1D, MaxPooling2D, Dropout, Flatten
+from tensorflow.keras.layers import Dense, Conv2D, Conv1D, MaxPooling2D, Dropout, Flatten, GlobalAveragePooling2D
 from tensorflow.keras import models
 from tensorflow.keras import utils
 
 # ===
+random_seed = 42
+random.seed(random_seed)
+np.random.seed(random_seed)
+
+# ===
 
 # circle, kite, parallelogram, rectangle, rhombus, square, trapezoid, triangle
-image_shape = (224, 224, 1)
 
 # ===
 
 def make_labels(directory, data=[], y_hat=[], label=0):
     for root, dirs, files in os.walk(directory):
         for file in files:
-            img = np.reshape(cv2.cvtColor(matplotlib.image.imread(directory+file), cv2.COLOR_RGB2GRAY),image_shape)
+            img = np.reshape(cv2.cvtColor(matplotlib.image.imread(directory+file), cv2.COLOR_RGB2GRAY),(224, 224, 1))
+            img = cv2.cvtColor(img, cv2.COLOR_GRAY2RGB)  # Convert to RGB
+            img = cv2.resize(img, (224, 224))
+
             data.append(img)
         y_hat = [label] * len(data)
     return np.array(data), np.array(y_hat)
@@ -42,44 +51,41 @@ y = np.hstack((y_circle, y_no_circle)).reshape(-1, 1)
 
 # ===
 
-classifier = Sequential()
-
-c1 = Conv2D(48, (3,3), padding='same', input_shape = image_shape, activation = 'relu')
-classifier.add(c1)
-classifier.add(MaxPooling2D(pool_size=(3, 3)))
-classifier.add(Dropout(0.5))
-classifier.add(Flatten()) 
-w1 = Dense(units = 32, activation = 'relu')
-w2 = Dense(units = 16, activation = 'relu')
-w3 = Dense(units = 4, activation = 'relu')
-w_end = Dense(units = 2, activation = 'softmax')
-classifier.add(w1)
-classifier.add(Dropout(0.5)) 
-classifier.add(w2)
-classifier.add(Dropout(0.5)) 
-classifier.add(w_end)
-# classifier.summary()
-
-# ===
-
-classifier.compile( optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['accuracy'])
-
-# ===
-
 y_cat = to_categorical(y)
-X_train, X_test, y_train, y_test = train_test_split(X, y_cat, test_size=.2, random_state=42)
-
-print(X_train.shape)
-print(y_train.shape)
+# Split your data into training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=random_seed)
 
 # ===
 
-history = classifier.fit(X_train, y_train, batch_size=16, epochs=2, verbose=1, validation_data=(X_test, y_test))
+# Load a pre-trained MobileNetV2 model as a feature extractor
+base_model = MobileNetV2(input_shape=(224, 224, 3), include_top=False, weights='imagenet')
 
-from keras.models import load_model
+# Create a custom head for classification
+classifier = Sequential()
+classifier.add(GlobalAveragePooling2D(input_shape=base_model.output_shape[1:]))
+classifier.add(Dropout(0.5))
+classifier.add(Dense(128, activation='relu'))
+classifier.add(Dropout(0.5))
+classifier.add(Dense(1, activation='sigmoid'))  # Binary classification (circle or not circle)
 
-classifier.save('shape-recognize-10epochs.h5')
+# Combine the base model and the custom head
+model = Sequential()
+model.add(base_model)
+model.add(classifier)
 
-scores = classifier.evaluate(X_test, y_test, verbose=1)
+# Set the layers in the base model as non-trainable (feature extractor)
+for layer in base_model.layers:
+    layer.trainable = False
+
+# Compile the model
+model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+
+# Train the model
+history = model.fit(X_train, y_train, batch_size=16, epochs=5, verbose=1, validation_data=(X_test, y_test))
+
+model.save('shape-recognizerv2-5eh.h5')
+
+# Evaluate the model
+scores = model.evaluate(X_test, y_test, verbose=1)
 print(f"Test loss: {scores[0]}")
 print(f"Test accuracy: {scores[1]}")
